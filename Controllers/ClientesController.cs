8fc7ff9fd0b1a7c74ac73ba3bf814d84e6633d12
Diego <dperedo@gmail.com>
2020-10-27 19:51:32 -0300

    actualizacion

--- Controllers/ClientesController.cs
+++ Controllers/ClientesController.cs
@@ -24,12 +24,13 @@ namespace Pedidos_back.Controllers
         }
 
         [HttpGet("query")]
-        public  IActionResult Todos1(string texto, int? take = 10, int? page = 1)
+        public  IActionResult Todos1(string texto, string order, int? take = 10, int? page = 1)
         {
             int skip = 0;
 
             if (!take.HasValue) take = 10;
             if (!page.HasValue) page = 1;
+            if(string.IsNullOrWhiteSpace(texto)) texto = null;
 
             //if ( string.IsNullOrWhiteSpace(page) || !int.TryParse(page, out _)) page = "1";
             //if ( string.IsNullOrWhiteSpace(take) || !int.TryParse(take, out _)) take = "10";
@@ -38,17 +39,75 @@ namespace Pedidos_back.Controllers
 
             skip = (page.Value - 1) * take.Value;
 
-            int count = repository.GetAll().Count();
+            var datos = repository.GetAll().Where(x => texto == null || 
+                x.RUT.ToLower().Contains(texto.ToLower()) || 
+                x.RazonSocial.ToLower().Contains(texto.ToLower())
+                ).OrderByDescending(x => x.FechaDeCreacion).Skip(skip)
+                .Take(Convert.ToInt32(take));
+
+            if (order == "FechaDeCreacionAcending") {  
+                datos = repository.GetAll().Where(x => texto == null || 
+                x.RUT.ToLower().Contains(texto.ToLower()) || 
+                x.RazonSocial.ToLower().Contains(texto.ToLower())
+                ).OrderBy(x => x.FechaDeCreacion).Skip(skip)
+                .Take(Convert.ToInt32(take)); }
+                else if (order == "FechaDeCreacionDescending") {  
+                datos = repository.GetAll().Where(x => texto == null || 
+                x.RUT.ToLower().Contains(texto.ToLower()) || 
+                x.RazonSocial.ToLower().Contains(texto.ToLower())
+                ).OrderByDescending(x => x.FechaDeCreacion).Skip(skip)
+                .Take(Convert.ToInt32(take)); }
+
+            if (order == "RazonSocialAcending") {  
+                datos = repository.GetAll().Where(x => texto == null || 
+                x.RUT.ToLower().Contains(texto.ToLower()) || 
+                x.RazonSocial.ToLower().Contains(texto.ToLower())
+                ).OrderBy(x => x.RazonSocial).Skip(skip)
+                .Take(Convert.ToInt32(take)); }
+                else if (order == "RazonSocialDescending") {  
+                datos = repository.GetAll().Where(x => texto == null || 
+                x.RUT.ToLower().Contains(texto.ToLower()) || 
+                x.RazonSocial.ToLower().Contains(texto.ToLower())
+                ).OrderByDescending(x => x.RazonSocial).Skip(skip)
+                .Take(Convert.ToInt32(take)); }
+
+            if (order == "RUTAcending") {  
+                datos = repository.GetAll().Where(x => texto == null || 
+                x.RUT.ToLower().Contains(texto.ToLower()) || 
+                x.RazonSocial.ToLower().Contains(texto.ToLower())
+                ).OrderBy(x => x.RUT).Skip(skip)
+                .Take(Convert.ToInt32(take)); }
+                else if (order == "RUTDescending") {  
+                datos = repository.GetAll().Where(x => texto == null || 
+                x.RUT.ToLower().Contains(texto.ToLower()) || 
+                x.RazonSocial.ToLower().Contains(texto.ToLower())
+                ).OrderByDescending(x => x.RUT).Skip(skip)
+                .Take(Convert.ToInt32(take)); }
+
+            if (order == "VigenteAcending") {  
+                datos = repository.GetAll().Where(x => texto == null || 
+                x.RUT.ToLower().Contains(texto.ToLower()) || 
+                x.RazonSocial.ToLower().Contains(texto.ToLower())
+                ).OrderBy(x => x.Vigente).Skip(skip)
+                .Take(Convert.ToInt32(take)); }
+                else if (order == "VigenteDescending") {  
+                datos = repository.GetAll().Where(x => texto == null || 
+                x.RUT.ToLower().Contains(texto.ToLower()) || 
+                x.RazonSocial.ToLower().Contains(texto.ToLower())
+                ).OrderByDescending(x => x.Vigente).Skip(skip)
+                .Take(Convert.ToInt32(take)); }
+
+            int count = repository.GetAll().Where(x => texto == null || 
+                x.RUT.ToLower().Contains(texto.ToLower()) || 
+                x.RazonSocial.ToLower().Contains(texto.ToLower())
+                ).Count();
 
             var result = new { total = count, 
                 take = take, 
                 page = page, 
                 skip = skip, 
-                list = repository.GetAll().Where(x => texto == null || 
-                x.RUT.ToLower().Contains(texto.ToLower()) || 
-                x.RazonSocial.ToLower().Contains(texto.ToLower())
-                ).Skip(skip)
-                .Take(Convert.ToInt32(take))};
+                numpages = Math.Ceiling(Convert.ToDecimal(count) / Convert.ToDecimal(take)),
+                list = datos};
 
             return Ok(result);
             // response = Ok(new {token = token}); x.RUT == texto
--- Controllers/ProductosController.cs
+++ Controllers/ProductosController.cs
@@ -20,7 +20,98 @@ namespace Pedidos_back.Controllers
             repository = _repository;
         }
 
+        [HttpGet("query")]
+        public  IActionResult Todos1(string texto, string order, int? take = 10, int? page = 1)
+        {
+            int skip = 0;
 
+            if (!take.HasValue) take = 10;
+            if (!page.HasValue) page = 1;
+            if(string.IsNullOrWhiteSpace(texto)) texto = null;
+
+            //if ( string.IsNullOrWhiteSpace(page) || !int.TryParse(page, out _)) page = "1";
+            //if ( string.IsNullOrWhiteSpace(take) || !int.TryParse(take, out _)) take = "10";
+
+            //if (Int32.Parse(page) > 0) skip = (Int32.Parse(page) - 1) * Convert.ToInt32(take);
+
+            skip = (page.Value - 1) * take.Value;
+
+            var datos = repository.GetAll().Where(x => texto == null || 
+                x.Codigo.ToLower().Contains(texto.ToLower()) || 
+                x.Nombre.ToLower().Contains(texto.ToLower())
+                ).OrderByDescending(x => x.FechaDeCreacion).Skip(skip)
+                .Take(Convert.ToInt32(take));
+
+            if (order == "FechaDeCreacionAcending") {
+                datos = repository.GetAll().Where(x => texto == null || 
+                x.Codigo.ToLower().Contains(texto.ToLower()) || 
+                x.Nombre.ToLower().Contains(texto.ToLower())
+                ).OrderBy(x => x.FechaDeCreacion).Skip(skip)
+                .Take(Convert.ToInt32(take));}
+                else if (order == "FechaDeCreacionDescending") {
+                datos = repository.GetAll().Where(x => texto == null || 
+                x.Codigo.ToLower().Contains(texto.ToLower()) || 
+                x.Nombre.ToLower().Contains(texto.ToLower())
+                ).OrderByDescending(x => x.FechaDeCreacion).Skip(skip)
+                .Take(Convert.ToInt32(take));
+                }
+
+            if (order == "NombreAcending") {
+                datos = repository.GetAll().Where(x => texto == null || 
+                x.Codigo.ToLower().Contains(texto.ToLower()) || 
+                x.Nombre.ToLower().Contains(texto.ToLower())
+                ).OrderBy(x => x.Nombre).Skip(skip)
+                .Take(Convert.ToInt32(take));}
+                else if (order == "NombreDescending") {
+                datos = repository.GetAll().Where(x => texto == null || 
+                x.Codigo.ToLower().Contains(texto.ToLower()) || 
+                x.Nombre.ToLower().Contains(texto.ToLower())
+                ).OrderByDescending(x => x.Nombre).Skip(skip)
+                .Take(Convert.ToInt32(take));
+                }
+
+            if (order == "PrecioAcending") {
+                datos = repository.GetAll().Where(x => texto == null || 
+                x.Codigo.ToLower().Contains(texto.ToLower()) || 
+                x.Nombre.ToLower().Contains(texto.ToLower())
+                ).OrderBy(x => x.Precio).Skip(skip)
+                .Take(Convert.ToInt32(take));}
+                else if (order == "PrecioDescending") {
+                datos = repository.GetAll().Where(x => texto == null || 
+                x.Codigo.ToLower().Contains(texto.ToLower()) || 
+                x.Nombre.ToLower().Contains(texto.ToLower())
+                ).OrderByDescending(x => x.Precio).Skip(skip)
+                .Take(Convert.ToInt32(take));
+                }
+
+            if (order == "VigenteAcending") {
+                datos = repository.GetAll().Where(x => texto == null || 
+                x.Codigo.ToLower().Contains(texto.ToLower()) || 
+                x.Nombre.ToLower().Contains(texto.ToLower())
+                ).OrderBy(x => x.Vigente).Skip(skip)
+                .Take(Convert.ToInt32(take));}
+                else if (order == "VigenteDescending") {
+                datos = repository.GetAll().Where(x => texto == null || 
+                x.Codigo.ToLower().Contains(texto.ToLower()) || 
+                x.Nombre.ToLower().Contains(texto.ToLower())
+                ).OrderByDescending(x => x.Vigente).Skip(skip)
+                .Take(Convert.ToInt32(take));
+                }
+
+            int count = repository.GetAll().Where(x => texto == null || 
+                x.Codigo.ToLower().Contains(texto.ToLower()) || 
+                x.Nombre.ToLower().Contains(texto.ToLower())
+                ).Count();
+
+            var result = new { total = count, 
+                take = take, 
+                page = page, 
+                skip = skip, 
+                numpages = Math.Ceiling(Convert.ToDecimal(count) / Convert.ToDecimal(take)),
+                list = datos};
+
+            return Ok(result);
+        }
         
     }
 }
